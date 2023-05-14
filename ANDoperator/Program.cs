e005a60730388c0ecb6a098f9544045e58574c07
SP <spukk@tktk.ee>
2023-05-14 12:21:21 +0300

    add week5 code

---
+++ ANDoperator/Program.cs
@@ -0,0 +1,21 @@
+﻿int math, biology, chemistry;
+
+Console.WriteLine("Enter your math results:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application could not be approved");
+}
---
+++ RKE132-week5/Program.cs
@@ -0,0 +1,13 @@
+﻿//AND - &&
+
+//true && true -- true
+//true && false -- true
+//false && true -- false
+// false && false -- false
+
+//OR II (pipes)
+l
+//true ll true -- ture
+//true II false -- true
+//false ii truse -- true
+//false II false-- false
